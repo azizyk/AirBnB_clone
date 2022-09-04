e15c64e2f8a6c4fe753be869f629ef8882d8295c
Abdelaziz Kassaw <azizikhero@gmail.com>
2022-09-04 14:34:31 +0300

    Created a user class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,9 @@
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
+from models.user import User
+
+classes = {"BaseModel": BaseModel,"User": User}
 
 
 class FileStorage:
